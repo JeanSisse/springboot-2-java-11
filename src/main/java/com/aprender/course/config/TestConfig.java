bb844191c0056e99f292b288e821612e58b06b4a
Jean Pierre <tavares1988@live.com>
2021-03-30 22:56:18 -0300

    Payment one-to-one association

--- src/main/java/com/aprender/course/config/TestConfig.java
+++ src/main/java/com/aprender/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.aprender.course.entities.Category;
 import com.aprender.course.entities.Order;
 import com.aprender.course.entities.OrderItem;
+import com.aprender.course.entities.Payment;
 import com.aprender.course.entities.Product;
 import com.aprender.course.entities.User;
 import com.aprender.course.entities.enums.OrderStatus;
@@ -104,5 +105,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2021-03-21T20:35:10Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
